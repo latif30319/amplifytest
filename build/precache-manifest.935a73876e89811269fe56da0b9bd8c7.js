c4bb70798de84436b57514fc5138bcd23d9ed3c0
Latif Abdul <latif30319@gmail.com>
2020-10-18 19:57:03 -0400

    initial commit

---
+++ build/precache-manifest.935a73876e89811269fe56da0b9bd8c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb04a286cd6d5e869dc9b31fabfe3c21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8101b75ac7fa0ee8db4c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fdc582785e99a615aead",
+    "url": "/static/js/2.c7db0818.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c7db0818.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8101b75ac7fa0ee8db4c",
+    "url": "/static/js/main.db11f7c4.chunk.js"
+  },
+  {
+    "revision": "e850977a9b7802797712",
+    "url": "/static/js/runtime-main.513e8507.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
