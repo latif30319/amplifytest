5c9ba305e2b1be31cd95a30ede5a36ed5f2565d7
Latif Abdul <latif30319@gmail.com>
2020-10-23 10:09:34 -0400

    Added API call to execute lambda function.

--- src/App.js
+++ src/App.js
@@ -1,63 +1,38 @@
-import React, { useState, useEffect } from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import { API } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
-import { listNotes } from './graphql/queries';
-import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
-
-const initialFormState = { name: '', description: '' }
 
 function App() {
-  const [notes, setNotes] = useState([]);
-  const [formData, setFormData] = useState(initialFormState);
+  //For rendering state
+  const [people, updatePeople] = useState([]); //Will create an empty Array, to be updated by updatePeople
+
+
+  //For invoking APIs
+  async function callApi(){
+    try {
+      const peopleData = await API.get('mainappapi', '/people');
+      console.log('peopleData: ', peopleData);
+      //Once data is back from the API call, update
+      updatePeople(peopleData.people);
+
+    }
+    catch(err) {
+      console.log({err})
+    }
+  }
 
   useEffect(() => {
-    fetchNotes();
-  }, []);
-
-  async function fetchNotes() {
-    const apiData = await API.graphql({ query: listNotes });
-    setNotes(apiData.data.listNotes.items);
-  }
-
-  async function createNote() {
-    if (!formData.name || !formData.description) return;
-    await API.graphql({ query: createNoteMutation, variables: { input: formData } });
-    setNotes([ ...notes, formData ]);
-    setFormData(initialFormState);
-  }
-
-  async function deleteNote({ id }) {
-    const newNotesArray = notes.filter(note => note.id !== id);
-    setNotes(newNotesArray);
-    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
-  }
+    callApi()},[])
 
   return (
     <div className="App">
-      <h1>My Notes App</h1>
-      <input
-        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
-        placeholder="Note name"
-        value={formData.name}
-      />
-      <input
-        onChange={e => setFormData({ ...formData, 'description': e.target.value})}
-        placeholder="Note description"
-        value={formData.description}
-      />
-      <button onClick={createNote}>Create Note</button>
-      <div style={{marginBottom: 30}}>
+      <h1>The Beatles People App</h1>
         {
-          notes.map(note => (
-            <div key={note.id || note.name}>
-              <h2>{note.name}</h2>
-              <p>{note.description}</p>
-              <button onClick={() => deleteNote(note)}>Delete note</button>
-            </div>
-          ))
+          people.map((p, i) => <h2>{p.name}</h2>)
         }
-      </div>
+
+
       <AmplifySignOut />
     </div>
   );
